ddd428378cd6614d700a01001914205aca858815
nducthang <nguyenducthang8a2@gmail.com>
2020-12-24 12:37:28 +0700

    Yolov5

---
+++ yolov5/data/gundata.py
@@ -0,0 +1,26 @@
+import glob
+import math
+import os
+import numpy as np
+
+files = []
+for ext in ["*.png", "*.jpeg", "*.jpg"]:
+    image_files = glob.glob(os.path.join("/media/thang/New Volume/Active-learning/yolov5/data/gun/", ext))
+    files += image_files
+
+nb_val = math.floor(len(files)*0.2)
+# Lấy ngẫu nhiên các số từ 0 đến len(files)-1
+# Lấy nb_val phần tử
+rand_idx = np.random.randint(0, len(files), nb_val)
+
+# Tao file train.txt
+with open("./data/train.txt", "w") as f:
+    for idx in np.arange(len(files)):
+        if (os.path.exists(files[idx][:-3]+"txt")):
+            f.write(files[idx]+"\n")
+
+# Tao file val.txt
+with open("./data/val.txt", "w") as f:
+    for idx in np.arange(len(files)):
+        if (idx in rand_idx) and (os.path.exists(files[idx][:-3]+"txt")):
+            f.write(files[idx]+"\n")
